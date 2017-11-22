a995645b09ed4aef4cfe4d636a174b2d7561c480
guqk <guqk@fosun.com>
2017-11-22 17:21:03 +0800

    update code

---
+++ source/devhelper-spring-boot-autoconfigure/src/main/java/com/github/jerryxia/devhelper/spring/boot/autoconfigure/DevHelperAutoConfiguration.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.github.jerryxia.devhelper.spring.boot.autoconfigure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author guqk
+ *
+ */
+@Configuration
+@EnableConfigurationProperties(MybatisProperties.class)
+public class DevHelperAutoConfiguration {
+
+}
---
+++ source/devhelper-spring-boot-autoconfigure/src/main/java/com/github/jerryxia/devhelper/spring/boot/autoconfigure/DevHelperProperties.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.github.jerryxia.devhelper.spring.boot.autoconfigure;
+
+/**
+ * @author guqk
+ *
+ */
+public class DevHelperProperties {
+
+}
