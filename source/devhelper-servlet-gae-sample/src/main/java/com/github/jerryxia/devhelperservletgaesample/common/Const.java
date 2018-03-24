53d21216e6db68dd0e4d44017005b7ca326b519f
JerryXia <gqkzwy@gmail.com>
2018-03-24 11:05:36 +0800

    gae sample

---
+++ source/devhelper-servlet-gae-sample/src/main/java/com/github/jerryxia/devhelperservletgaesample/common/Const.java
@@ -0,0 +1,24 @@
+package com.github.jerryxia.devhelperservletgaesample.common;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+public class Const {
+
+    private static final Logger logger = LoggerFactory.getLogger(Const.class);
+
+    public static Properties AppProperties = null;
+
+    static {
+        try {
+            AppProperties = PropertiesLoaderUtils.loadAllProperties("app.properties");
+        } catch (IOException e) {
+            logger.error("load app.properties error", e);
+        }
+    }
+
+}
---
+++ source/devhelper-servlet-gae-sample/src/main/java/com/github/jerryxia/devhelperservletgaesample/web/CustomHandlerExceptionResolver.java
@@ -0,0 +1,34 @@
+package com.github.jerryxia.devhelperservletgaesample.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+public class CustomHandlerExceptionResolver extends SimpleMappingExceptionResolver {
+
+    @Override
+    protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
+            Exception ex) {
+        String viewName = determineViewName(ex, request);
+        if (viewName != null) {
+            Integer statusCode = determineStatusCode(request, viewName);
+            if (statusCode != null) {
+                String acceptHeaderValue = request.getHeader("accept");
+                if (acceptHeaderValue != null && acceptHeaderValue.indexOf("application/json") > -1) {
+                    ModelAndView jsonMv = new ModelAndView();
+                    jsonMv.addObject("code", 0);
+                    jsonMv.addObject("msg", ex.getMessage());
+                    jsonMv.addObject(DEFAULT_EXCEPTION_ATTRIBUTE, null);
+                    return jsonMv;
+                } else {
+                    applyStatusCodeIfPossible(request, response, statusCode);
+                }
+            }
+            return getModelAndView(viewName, ex, request);
+        } else {
+            return null;
+        }
+    }
+}
