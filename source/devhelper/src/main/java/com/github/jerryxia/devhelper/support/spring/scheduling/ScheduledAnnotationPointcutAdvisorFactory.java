14d4fe747871a8cae2065118b100dc021478ace2
JerryXia <gqkzwy@gmail.com>
2025-08-09 18:31:09 +0800

    feat: add Annotation: "@Schedules" supported.

--- source/devhelper/src/main/java/com/github/jerryxia/devhelper/support/spring/scheduling/ScheduledAnnotationPointcutAdvisorFactory.java
+++ source/devhelper/src/main/java/com/github/jerryxia/devhelper/support/spring/scheduling/ScheduledAnnotationPointcutAdvisorFactory.java
@@ -3,14 +3,19 @@
  */
 package com.github.jerryxia.devhelper.support.spring.scheduling;
 
+import org.springframework.aop.support.ComposablePointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
 
 public class ScheduledAnnotationPointcutAdvisorFactory {
 
     public static DefaultPointcutAdvisor getScheduledAdvisor() {
-        AnnotationMatchingPointcut pointcut = AnnotationMatchingPointcut.forMethodAnnotation(Scheduled.class);
+        AnnotationMatchingPointcut scheduledPointcut = AnnotationMatchingPointcut.forMethodAnnotation(Scheduled.class);
+        AnnotationMatchingPointcut schedulesPointcut = AnnotationMatchingPointcut.forMethodAnnotation(Schedules.class);
+        ComposablePointcut pointcut = new ComposablePointcut(scheduledPointcut).union(schedulesPointcut);
+
         DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, null);
         return advisor;
     }
